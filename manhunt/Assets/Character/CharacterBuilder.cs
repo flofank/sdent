1dc97fa17cb53367dd5718b77bcbdaaba53ae2db
Florian Fankhauser <fankiflo@gmail.com>
2016-06-04 17:43:19 +0200

    started to implement character generation

--- manhunt/Assets/Character/CharacterBuilder.cs
+++ manhunt/Assets/Character/CharacterBuilder.cs
@@ -14,15 +14,24 @@ public class CharacterBuilder : MonoBehaviour {
 
     public void walk1()
     {
-        transform.Find("body").gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("char_walk0_body");
-        transform.Find("trousers").gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("char_walk0_trousers_oliv");
-        transform.Find("top").gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("char_walk0_top_shirtgrayorange");
+        transform.Find("body").gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("characters/char_walk0_body");
+        transform.Find("trousers").gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("characters/char_walk0_trousers_oliv");
+        transform.Find("top").gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("characters/char_walk0_top_shirtgrayorange");
     }
 
     public void walk2()
     {
-        transform.Find("body").gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("char_walk1_body");
-        transform.Find("trousers").gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("char_walk1_trousers_oliv");
-        transform.Find("top").gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("char_walk1_top_shirtgrayorange");
+        transform.Find("body").gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("characters/char_walk1_body");
+        transform.Find("trousers").gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("characters/char_walk1_trousers_oliv");
+        transform.Find("top").gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("characters/char_walk1_top_shirtgrayorange");
+    }
+
+    private enum Beard {
+        black0, black1, black2, black3, black4, black5, black6, black7, black8, black9,
+        blond0, blond1, blond2, blond3, blond4, blond5, blond6, blond7, blond8, blond9,
+        red0, red1, red2, red3, red4, red5, red6, red7, red8, red9,
+        brown0, brown1, brown2, brown3, brown4, brown5, brown6, brown7, brown8, brown9
+    }
+    private enum BeardType {
     }
 }
---
+++ manhunt/Assets/Scripts/UIController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+    public GameObject File;
+
+	// Use this for initialization
+	void Start () {
+        File.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void showFile()
+    {
+        File.SetActive(true);
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("scene1");
+    }
+}
